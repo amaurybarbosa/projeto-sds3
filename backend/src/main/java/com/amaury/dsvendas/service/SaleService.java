210d077329b85cffbd7bc37e409f3f66ddb3d4ec
Amaury <amauryfvjunior@gmail.com>
2021-09-11 16:20:38 -0300

    Pagination 2

--- backend/src/main/java/com/amaury/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/amaury/dsvendas/service/SaleService.java
@@ -1,14 +1,17 @@
 package com.amaury.dsvendas.service;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.amaury.dsvendas.dto.SaleDTO;
 import com.amaury.dsvendas.entities.Sale;
 import com.amaury.dsvendas.repositories.SaleRepository;
+import com.amaury.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -16,7 +19,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
